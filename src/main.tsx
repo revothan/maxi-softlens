c5af892b85a8f7f5dbed21bc71a7f3a8a0cf9f58
0x0than <136307533+revothan@users.noreply.github.com>
2025-05-05 12:20:45 +0700

    Fix import path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-import { CartProvider } from './contexts/CartContext.tsx';
+import { CartProvider } from './contexts/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
